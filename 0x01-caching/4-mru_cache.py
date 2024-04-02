4c8649c32b70ecdb2805a8833a3c5d0af26f4957
mwanyambu <jmwanyambu@gmail.com>
2024-04-02 21:23:23 +0000

    mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -15,15 +15,16 @@ class MRUCache(BaseCaching):
 
     def put(self, key, item):
         """ adds items to cache """
-        if key is not None or item is not None:
-            if key not in self.cache_data:
-                if len(self.cache_data) + 1 > self.MAX_ITEMS:
-                    most_recent, _ = self.cache_data.popitem(False)
-                    print("DISCARD:", most_recent)
-                self.cache_data[key] = item
-                self.cache_data.move_to_end(key, last=False)
-            else:
-                self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > self.MAX_ITEMS:
+                most_recent, _ = self.cache_data.popitem(False)
+                print("DISCARD:", most_recent)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ gets item by key """
