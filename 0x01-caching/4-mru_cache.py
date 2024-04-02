f1f28a96da256f3da0255bfda588fda093fc67e6
mwanyambu <jmwanyambu@gmail.com>
2024-04-02 21:20:47 +0000

    MRU

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """MRUCache """
-
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
@@ -11,24 +11,23 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ initializes MRUCache """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ adds items to cache """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                del self.cache_data[key]
-
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                most_recent = list(self.cache_data.keys())[-1]
-                print("DISCARD:", most_recent)
-                del self.cache_data[most_recent]
-            self.cache_data[key] = item
+        if key is not None or item is not None:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > self.MAX_ITEMS:
+                    most_recent, _ = self.cache_data.popitem(False)
+                    print("DISCARD:", most_recent)
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, last=False)
+            else:
+                self.cache_data[key] = item
 
     def get(self, key):
         """ gets item by key """
         if key is not None:
             if key in self.cache_data:
-                value = self.cache_data.pop(key)
-                self.cache_data[key] = value
-                return value
-        return None
+                self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
